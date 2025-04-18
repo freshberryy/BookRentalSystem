d06372f845a7d4d3fecbdf05dd065150ad4ff80f
freshberryy <161305732+freshberryy@users.noreply.github.com>
2025-04-18 23:33:27 +0900

    update

--- del_and_add_records.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
-input_path = os.path.join(current_dir, 'books.csv') 
-df = pd.read_csv(input_path, encoding='utf-8-sig')
-df_10k = df.head(10000)
-df_duplicated = pd.concat([df_10k, df_10k], ignore_index=True)
-output_path = os.path.join(current_dir, 'books.csv')
-df_duplicated.to_csv(output_path, index=False)
--- extract_num__and_del_record.py
+++ extract_num__and_del_record.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import re
 current_dir = os.path.dirname(os.path.abspath(__file__))
-input_file = os.path.join(current_dir, 'b.csv')  # 실제 파일명으로 바꿔주세요
+input_file = os.path.join(current_dir, 'b.csv')  
 output_file = os.path.join(current_dir, 'books.csv')
 df = pd.read_csv(input_file, encoding='cp949')
 def clean_year(value):
@@ -12,4 +12,7 @@ def clean_year(value):
     return int(digits) if digits else 0
 
 df['출판년'] = df['출판년'].apply(clean_year)
+
+df = df.sample(n=1000, random_state=42)
+
 df.to_csv(output_file, index=False, encoding='cp949')
